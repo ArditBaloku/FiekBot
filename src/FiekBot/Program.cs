af456e49ee4408d954b305735551b9846eccac76
Fatbardh Kadriu <fatbardh.kadriu@gmail.com>
2019-03-21 22:19:19 +0100

    Update Program.cs

--- src/FiekBot/Program.cs
+++ src/FiekBot/Program.cs
@@ -7,6 +7,7 @@ namespace FiekBot
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World from fiekbot!");
+            Console.ReadKey();
         }
     }
 }
