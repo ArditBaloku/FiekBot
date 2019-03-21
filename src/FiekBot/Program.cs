95939c93174c64796c7f89cb8ec14b8f482194c8
Edon Gashi <edon.gashi@uni-pr.edu>
2019-03-21 21:54:28 +0100

    hello

--- src/FiekBot/Program.cs
+++ src/FiekBot/Program.cs
@@ -6,7 +6,7 @@ namespace FiekBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World from fiekbot!");
         }
     }
 }
